b709ff276a648fc8047714dfc13854b7b24906fb
Serhiy <sergiy.kusiv@gmail.com>
2025-05-31 19:46:07 +0300

    change mistake in my loop | Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,7 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Lottery lot = new Lottery();
         for (int i = 0; i < 3; i++) {
-            lot.getRandomBall();
             System.out.println(lot.getRandomBall().toString());
         }
     }
