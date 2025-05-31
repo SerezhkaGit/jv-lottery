0ee8089ec9673467f77bcda6a1d981f012c9d7ed
Serhiy <sergiy.kusiv@gmail.com>
2025-05-31 19:04:44 +0300

    Added solution to getRandomColot() method in ColorSupplier + change access modificator  to public (enum) in Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,4 +1,41 @@
 package core.basesyntax;
 
 public class Ball {
+    public enum Colors{
+        white,
+        black,
+        red,
+        blue,
+        green,
+        brown,
+        yellow
+    }
+
+    private  int number;
+    private  Colors color;
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "number=" + number +
+                ", color=" + color +
+                '}';
+    }
+
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        Ball.Colors[] allColors = Ball.Colors.values();
+        Ball.Colors randomColor = allColors[new Random().nextInt(allColors.length)];
+        return randomColor.toString();
     }
 }
